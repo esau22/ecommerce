bf481a2d794aa51e635fc2f7724866419a47f06b
esau22 <emr.emr.espiritu@gmail.com>
2024-09-16 11:48:49 -0500

    finally-4

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET({ params }: { params: { id: number } }) {
   const { id } = params;
 
   try {
