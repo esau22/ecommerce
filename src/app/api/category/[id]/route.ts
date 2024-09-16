461880e124271f23f8ab23665064c272ae994092
esau22 <emr.emr.espiritu@gmail.com>
2024-09-16 12:22:43 -0500

    finally-7

--- src/app/api/category/[id]/route.ts
+++ src/app/api/category/[id]/route.ts
@@ -1,41 +1,48 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function Handler(
+export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: { id?: string } } // `id` es opcional
 ) {
-  const { id } = params;
   try {
-    // Convertir id a número
-    const categoryId = Number(id);
+    // Si se proporciona un ID en los parámetros de la ruta
+    if (params.id) {
+      const categoryId = Number(params.id);
 
-    // Verificar si la conversión es válida
-    if (isNaN(categoryId)) {
-      return NextResponse.json(
-        { error: "ID de categoría no válido" },
-        { status: 400 }
-      );
+      // Verificar si la conversión es válida
+      if (isNaN(categoryId)) {
+        return NextResponse.json(
+          { error: "ID de categoría no válido" },
+          { status: 400 }
+        );
+      }
+
+      // Buscar la categoría por ID
+      const category = await prisma.category.findUnique({
+        where: {
+          id: categoryId, // Si el id es de tipo number, sino ajusta el tipo.
+        },
+      });
+
+      // Si no se encuentra la categoría
+      if (!category) {
+        return NextResponse.json(
+          { error: "Categoría no encontrada" },
+          { status: 404 }
+        );
+      }
+
+      return NextResponse.json(category, { status: 200 });
+    } else {
+      // Si no se proporciona un ID, obtener todas las categorías
+      const categories = await prisma.category.findMany();
+
+      return NextResponse.json(categories, { status: 200 });
     }
-    // Buscar la categoría por ID
-    const category = await prisma.category.findUnique({
-      where: {
-        id: categoryId, // Si el id es de tipo number, sino ajusta el tipo.
-      },
-    });
-
-    // Si no se encuentra la categoría
-    if (!category) {
-      return NextResponse.json(
-        { error: "Categoría no encontrada" },
-        { status: 404 }
-      );
-    }
-
-    return NextResponse.json(category, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      { error: "Error al recuperar la categoría" },
+      { error: "Error al recuperar las categorías" },
       { status: 500 }
     );
   }
