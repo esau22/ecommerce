8690cd52fcb765a9125977dad0e930056273d6e3
esau22 <emr.emr.espiritu@gmail.com>
2024-09-17 14:59:44 -0500

    feat-01

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,35 +1,35 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const fetchCategory = async (id?: number) => {
   const url = id
-    ? `${API_URL}/api/categories/${id}` // Si se pasa un id, busca por categoría específica
-    : `${API_URL}/api/categories`; // Si no, obtiene todas las categorías
+    ? `/api/categories/${id}` // Ruta relativa para obtener una categoría específica
+    : `/api/categories`; // Ruta relativa para obtener todas las categorías
   const response = await fetch(url);
   return response.json();
 };
 
 export const fetchCustomer = async () => {
-  const response = await fetch(`${API_URL}/api/customer`);
+  const response = await fetch(`/api/customer`); // Ruta relativa
   return response.json();
 };
 
 export const fetchProduct = async (id?: number) => {
   const url = id
-    ? `${API_URL}/api/products/${id}` // Si se pasa un id, busca por categoría específica
-    : `${API_URL}/api/products`; // Si no, obtiene todas las categorías
+    ? `/api/products/${id}` // Ruta relativa para obtener un producto específico
+    : `/api/products`; // Ruta relativa para obtener todos los productos
   const response = await fetch(url);
   return response.json();
 };
 
 export const fetchReport = async () => {
-  const response = await fetch(`${API_URL}/api/report`);
+  const response = await fetch(`/api/report`); // Ruta relativa
   return response.json();
 };
+
 export const fetchSupplier = async () => {
-  const response = await fetch(`${API_URL}/api/supplier`);
+  const response = await fetch(`/api/supplier`); // Ruta relativa
   return response.json();
 };
 
 export const fetchUser = async () => {
-  const response = await fetch(`${API_URL}/api/user`);
+  const response = await fetch(`/api/user`); // Ruta relativa
   return response.json();
 };
