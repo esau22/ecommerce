f7b3aa8e5d54fbd47c57fed2b5576f66870fe0d5
esau22 <emr.emr.espiritu@gmail.com>
2024-09-17 10:42:57 -0500

    feat-finsh

--- src/app/(admin)/dashboard/sales/[id]/page.tsx
+++ src/app/(admin)/dashboard/sales/[id]/page.tsx
@@ -1,10 +1,9 @@
 import FormSales from "@/components/shared/form/form-sales";
 import { fetchCategory, fetchProduct } from "@/lib/fetch";
-import { Category, Product } from "@/types/types";
 
 const MakeSales = async () => {
-  const category: Category[] = await fetchCategory();
-  const products: Product[] = await fetchProduct();
+  const category = await fetchCategory();
+  const products = await fetchProduct();
   return (
     <div className="flex flex-col gap-10">
       <FormSales category={category} products={products} />
